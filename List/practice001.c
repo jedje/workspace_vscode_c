dad3bf4c03be66c32afee32c1cf3c0be088e9788
jedje <jedje95@gamil.com>
2021-12-06 19:11:41 +0900

    first commit

---
+++ List/practice001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    
+    int i;
+    char string1[20] = "Dreams come true!", string2[20], *ptr1, *ptr2;
+
+    ptr1 = string1;
+    printf("\n string1 address = %u \t ptr1 = %u \n", string1, ptr1);
+    
+    printf("\n");
+    //printf("%", string1[0]);
+
+    //getchar();    
+
+}   
